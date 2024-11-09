8e47a098ef1a1511632cfeca6af13baa68436e56
jhdgo1225 <parkjh99365@gmail.com>
2024-11-09 22:11:27 +0900

    BOJ 9455 Problem is resolved with One Direction Array

--- baekjoon/Implementation/9455.cpp
+++ baekjoon/Implementation/9455.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int m, n, board[101][101];
-int cnt;
-void tiltDown(int col)
-{
-	int idx = m-1;
-	for (int row=m-1; row>=0; row--)
-	{
-		if (!board[row][col]) continue;
-		if (board[idx][col]) idx--;
-		else if (board[row][col]) {
-			board[idx][col] = board[row][col];
-			board[row][col] = 0;
-			cnt += (idx-- - row);
-		}
-	}
-}
 int main()
 {
-	int t;cin>>t;
-	while (t--)
+	int T; cin>>T;
+	while (T--)
 	{
+		int cnt[101] = { 0 };
+		int m, n, tmp, res = 0;
 		cin >> m >> n;
-		for (int i=0; i<m; i++)
-			for (int j=0; j<n; j++)
-				cin >> board[i][j];
-		for (int i=0; i<n; i++)
-			tiltDown(i);
-		cout << cnt << '\n';
-		cnt = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> tmp;
+				if (tmp == 1) cnt[j]++;
+				else res += cnt[j];
+			}
+		}
+		cout << res << '\n';
 	}
+	return 0;
 }
