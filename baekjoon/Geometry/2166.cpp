2adfa3253ce1024c043ea45305a2ebaedb136168
jhdgo1225 <parkjh99365@gmail.com>
2025-03-03 20:40:38 +0900

    Three geometry problems are solved

---
+++ baekjoon/Geometry/2166.cpp
@@ -0,0 +1,29 @@
+// 참고 사이트: https://www.mathopenref.com/coordpolygonarea2.html
+#include <iostream>
+using namespace std;
+
+double X[10001];
+double Y[10001];
+double polygonArea(int numPoints) 
+{ 
+	double area = 0;
+	int j = numPoints-1;
+
+	for (int i=0; i<numPoints; i++)
+	{
+		area += (X[j] + X[i]) * (Y[j]-Y[i]); 
+		j = i;
+	}
+	return abs(area)/2;
+}
+
+int main()
+{
+	int N; cin >> N;
+	for (int i=0; i<N; i++) {
+		cin >> X[i] >> Y[i];
+	}
+	cout << fixed;
+	cout.precision(1);
+	cout << polygonArea(N);
+}
---
+++ baekjoon/Geometry/32840.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <numeric>
+#include <deque>
+using namespace std;
+using ll = long long;
+
+struct coordinate {
+	ll x;
+	ll y;
+};
+
+coordinate avail[3][2];
+ll minRes = LONG_MAX;
+ll maxRes = LONG_MIN;
+deque<coordinate> q;
+
+void calcArea()
+{
+	ll area = abs((q[1].x - q[0].x) * (q[2].y - q[0].y) - (q[2].x - q[0].x) * (q[1].y - q[0].y));
+	minRes = min(minRes, area);
+	maxRes = max(maxRes, area);
+}
+
+void dfs(int depth)
+{
+	if (depth == 3) {
+		calcArea();
+		return ;
+	}
+	for (int i=0; i<2; i++) {
+		q.push_back(avail[depth][i]);
+		dfs(depth + 1);
+		q.pop_back();
+	}
+}
+
+int main()
+{
+	coordinate coords[3];
+	for (int i=0; i<3; i++) cin >> coords[i].x >> coords[i].y;
+	for (int i=0; i<3; i++) {
+		ll diffX = coords[(i + 1) % 3].x - coords[i].x;
+		ll diffY = coords[(i + 1) % 3].y - coords[i].y;
+		ll gcdTwoCoords = gcd(abs(diffX), abs(diffY));
+		if (gcdTwoCoords == 1) {
+			cout << -1;
+			return (0);
+		}
+		ll gapCoordsX = diffX / gcdTwoCoords;
+		ll gapCoordsY = diffY / gcdTwoCoords;
+		avail[i][0].x = coords[i].x + gapCoordsX;
+		avail[i][0].y = coords[i].y + gapCoordsY;
+		avail[i][1].x = coords[(i + 1) % 3].x - gapCoordsX;
+		avail[i][1].y = coords[(i + 1) % 3].y - gapCoordsY;
+	}
+	dfs(0);
+	cout << maxRes << ' ' << minRes;
+}
