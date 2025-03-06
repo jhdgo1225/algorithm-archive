640f83ada877d232aaa33d7ea9415271c4fac73b
JONGHO PARK <94268538+jhdgo1225@users.noreply.github.com>
2025-03-06 16:15:51 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.cc
+++ 백준/Gold/9084. 동전/동전.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int coins[21];
-int dp[21][10001];
+int dp[10001];
 int main()
 {
 	int T, N, M; cin >> T;
@@ -10,20 +10,13 @@ int main()
 		cin >> N;
 		for (int i=1; i<=N; i++) cin >> coins[i];
 		cin >> M;
-		for (int i=1; i<=M; i++)
-			dp[1][i] = (i % coins[1] == 0);
-		for (int i=2; i<=N; i++) {
+		for (int i=1; i<=N; i++) {
 			for (int j=1; j<=M; j++) {
-				dp[i][j] = dp[i-1][j] + (j % coins[i] == 0);
-				int tmp = 1;
-				while (j > coins[i] * tmp) {
-					dp[i][j] += dp[i-1][j - coins[i] * tmp];
-					tmp += 1;
-				}
+				if (j - coins[i] > 0) dp[j] += dp[j - coins[i]];
+				else if (j - coins[i] == 0) dp[j]++;
 			}
 		}
-		cout << dp[N][M] << '\n';
-		for (int i=1; i<=N; i++)
-			fill(dp[i] + 1, dp[i] + M + 1, 0);
+		cout << dp[M] << '\n';
+		fill(dp + 1, dp + M + 1, 0);
 	}
 }
