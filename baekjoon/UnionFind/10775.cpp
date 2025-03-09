b68282e931b2423e354b54d39902614bdf6c6162
jhdgo1225 <parkjh99365@gmail.com>
2025-03-09 18:36:07 +0900

    Airport is mine

--- baekjoon/UnionFind/10775.cpp
+++ baekjoon/UnionFind/10775.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int arr[1000001];
+int group[100001];
+int arr[100001];
+
+int find(int x) {
+	if (group[x] < 0) {
+		group[x] = x;
+		return x;
+	}
+	if (x == 0) return 0;
+	return group[x] = find(group[x] - 1);
+}
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
+	int G, P, g, i; cin >> G >> P;
+	fill(group + 1, group + 1 + G, -1);
+	for (i=0; i<P; i++) cin >> arr[i];
+	for (i=0; i<P; i++) {
+		find(arr[i]);
+		if (group[arr[i]] == 0) break;
+	}
+	cout << i;
 }
