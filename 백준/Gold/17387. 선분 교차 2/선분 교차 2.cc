18fc44173fe4d8ab8b29fc6bd1fce5e05f00b46c
JONGHO PARK <94268538+jhdgo1225@users.noreply.github.com>
2025-03-09 20:08:20 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -15,11 +15,8 @@ struct line {
 	point p1, p2;
 };
 
-line lines[2];
-
 int ccw(point &p1, point &p2, point &p3) {
-	ll res = (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - \
-		(p2.x * p1.y + p3.x * p2.y + p1.x * p3.y);
+	ll res = (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y);
 	if (res > 0) return 1;
 	if (res < 0) return -1;
 	return 0;
@@ -38,7 +35,8 @@ bool lineIntersect(line &l1, line &l2) {
 
 int main()
 {
-	for (int i=0; i<2; i++)
-		cin >> lines[i].p1.x >> lines[i].p1.y >> lines[i].p2.x >> lines[i].p2.y;
-	cout << lineIntersect(lines[0], lines[1]);
+	line l1, l2;
+	cin >> l1.p1.x >> l1.p1.y >> l1.p2.x >> l1.p2.y;
+	cin >> l2.p1.x >> l2.p1.y >> l2.p2.x >> l2.p2.y;
+	cout << lineIntersect(l1, l2);
 }
