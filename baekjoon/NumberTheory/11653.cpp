4946a36e599c66de65de335a8a58e64f832770aa
jhdgo1225 <parkjh99365@gmail.com>
2025-11-20 21:05:11 +0900

    REFACTOR: Baekjoon 11653 problem is refactored

--- baekjoon/NumberTheory/11653.cpp
+++ baekjoon/NumberTheory/11653.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
-#include <list>
 #include <cmath>
 using namespace std;
 
-list<pair<int, int>> primeFactorization(int N)
+void primeFactorization(int N)
 {
-	list<pair<int, int>> res;
 	int root = int(sqrt(N));
 	for (int i=2; i<=root + 1; i++)
 	{
 		while (N % i == 0) {
-			if (res.empty() || res.back().first != i) res.push_back({i, 1});
-            else res.back().second += 1;
+			cout << i << '\n';
 			N /= i;
 		}
 	}
-    if (N != 1)
-		res.push_back({N, 1});
-	return res;
+    if (N != 1) cout << N << '\n';
 }
 
 int main()
 {
 	int N; cin >> N;
-	list<pair<int, int>> res = primeFactorization(N);
-	for (auto it: res)
-		for (int i=0; i<it.second; i++)
-			cout << it.first << '\n';
+	primeFactorization(N);
 }
