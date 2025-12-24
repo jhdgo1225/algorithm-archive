fe1f296bcc1558495c24d554a8480af5f7384f4e
JONGHO PARK <94268538+jhdgo1225@users.noreply.github.com>
2025-12-25 00:02:59 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,9 @@
+SELECT ANIMAL_ID, NAME,
+    CASE
+        WHEN SUBSTRING_INDEX(SEX_UPON_INTAKE, ' ', 1)='Neutered' OR
+            SUBSTRING_INDEX(SEX_UPON_INTAKE, ' ', 1)='Spayed'
+            THEN 'O'
+        ELSE 'X'
+    END AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
