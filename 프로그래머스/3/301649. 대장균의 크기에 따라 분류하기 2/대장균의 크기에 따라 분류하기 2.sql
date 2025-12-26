a1ff3c861bec532ae3b8e570d78057a66956e1f0
jhdgo1225 <parkjh99365@gmail.com>
2025-12-26 18:54:53 +0900

    SQL: ANOTHER SOLVE METHOD

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -12,3 +12,22 @@ FROM (
     FROM ECOLI_DATA
 ) AS CUSTOM_TB
 ORDER BY ID;
+
+# ANOTHER SOLVE METHOD(It's bad thing)
+-- SELECT
+--     ID,
+--     IF(
+--         ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) <= (COUNT(*) OVER () / 4), 
+--         "CRITICAL",
+--         IF (
+--             ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) <= (COUNT(*) OVER () * 2 / 4), 
+--             "HIGH",
+--             IF (
+--                 ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) <= (COUNT(*) OVER () * 3 / 4), 
+--                 "MEDIUM",
+--                 "LOW"
+--             )
+--         )
+--     ) AS COLONY_NAME
+-- FROM ECOLI_DATA
+-- ORDER BY ID ASC;
