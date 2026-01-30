ffda9d46dba55a7fcb52978a7aec4c883e474dc4
JONGHO PARK <94268538+jhdgo1225@users.noreply.github.com>
2026-01-30 17:37:57 +0900

    [Gold V] Title: 괄호의 값, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -5,41 +5,34 @@ input = sys.stdin.readline
 def main():
 	brackets = input().strip()
 	st_brackets = []
-	st_numbers = []
-	flag = 0
-	for bracket in brackets:
-		if bracket == '(' or bracket == '[':
-			tmp = 1
+	result = 0
+	mul = 1
+	for idx, bracket in enumerate(brackets):
+		if bracket == '(':
+			mul *= 2
 			st_brackets.append(bracket)
-		else:
-			tmp2 = 0
-			while (len(st_brackets) != 0 and st_brackets[-1] == '.'):
-				tmp2 += st_numbers[-1]
-				st_brackets.pop()
-				st_numbers.pop()
-			if len(st_brackets) == 0:
-				flag = 1
+		elif bracket == '[':
+			mul *= 3
+			st_brackets.append(bracket)
+		elif bracket == ']':
+			if not len(st_brackets) or st_brackets[-1] != '[':
+				result = 0
 				break
-			if tmp2 > 0:
-				tmp = tmp2
-			if bracket == ')' and st_brackets[-1] == '(':
-				tmp *= 2
-			elif bracket == ']' and st_brackets[-1] == '[':
-				tmp *= 3
-			else:
-				flag = 1
-				break
-			st_numbers.append(tmp)
+			if brackets[idx - 1] == '[':
+				result += mul
 			st_brackets.pop()
-			st_brackets.append('.')
-	res = 0
-	while (len(st_brackets) and st_brackets[-1] == '.'):
-		res += st_numbers[-1]
-		st_brackets.pop()
-		st_numbers.pop()
-	if (flag or len(st_brackets)):
+			mul //= 3
+		elif bracket == ')':
+			if not len(st_brackets) or st_brackets[-1] != '(':
+				result = 0
+				break
+			if brackets[idx - 1] == '(':
+				result += mul
+			st_brackets.pop()
+			mul //= 2
+	if len(st_brackets):
 		print(0)
 	else:
-		print(res)
+		print(result)
 if __name__ == "__main__":
 	main()
