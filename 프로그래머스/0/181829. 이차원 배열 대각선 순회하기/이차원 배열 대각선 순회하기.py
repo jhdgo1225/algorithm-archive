567f7af3fdf915eadd1075ac7b98f5fb45afd868
jhdgo1225 <parkjh99365@gmail.com>
2026-03-02 16:27:28 +0900

    No.181829 problem's logic is simplified

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -1,3 +1,5 @@
 def solution(board, k):
-    return sum([board[i][j] for i in range(0, min(k+1, len(board))) for j in range(0, min(k - i + 1, len(board[0])))])
-print(solution([[0, 1, 2],[1, 2, 3],[2, 3, 4],[3, 4, 5]], 2))
+    return sum(board[i][j]
+               for i in range(len(board))
+               for j in range(len(board[0]))
+               if i + j <= k)
