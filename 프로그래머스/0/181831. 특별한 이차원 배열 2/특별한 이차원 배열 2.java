d90237cb48d3906ada46b2e9f45135c117897be7
jhdgo1225 <parkjh99365@gmail.com>
2026-03-02 17:09:44 +0900

    No.181831 Problem is changed into JAVA Stream API

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,12 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[][] arr) {
-        for (int i=0; i<arr.length; i++) {
-            for (int j=0; j<=i; j++) {
-                if (arr[i][j] != arr[j][i]) {
-                    return 0;
-                }
-            }
-        }
-        return 1;
+        return IntStream.range(0, arr.length)
+            .allMatch(i -> IntStream.range(0, i + 1)
+                .allMatch(j -> arr[i][j] == arr[j][i])) ? 1 : 0;
     }
 }
