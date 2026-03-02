2b306bebb435f91fbc15512ac2890794dd9a8dd6
jhdgo1225 <parkjh99365@gmail.com>
2026-03-02 15:44:04 +0900

    more simple logic

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,9 +1,6 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String string_zero_to_s = my_string.substring(0, s);
-        String overwrite_to_my_string_remainder = my_string.substring(s+overwrite_string.length(), my_string.length());
-        return string_zero_to_s + overwrite_string + overwrite_to_my_string_remainder;
+        int end = Math.min(s + overwrite_string.length(), my_string.length());
+        return my_string.substring(0, s) + overwrite_string + my_string.substring(end);
     }
 }
