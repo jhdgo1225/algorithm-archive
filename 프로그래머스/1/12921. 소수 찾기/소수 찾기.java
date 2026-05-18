dddd73f3cee6daf4ef72246879a4c87af0229477
JONGHO PARK <94268538+jhdgo1225@users.noreply.github.com>
2026-05-18 14:28:26 +0900

    [level 1] Title: 소수 찾기, Time: 102.17 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,22 +1,18 @@
-
-
 class Solution {
-    public int solution(int n) {
-        boolean[] seive = new boolean[n + 1];
-        for (int i=0; i<=n; i++) seive[i] = true;
-        seive[0] = seive[1] = false;
-        for (int i=2; i<=n; i++) {
-            if (!seive[i]) continue;
-            for (int j=2; i*j<=n; j++) {
-                seive[i*j] = false;
-            }
-        }
-        int answer = 0;
-        for (int i=1; i<=n; i++) {
-            if (seive[i]) {
-                answer += 1;
-            }
-        }
-        return answer;
-    }
+  public int solution(int n) {
+      int answer = 0;
+      for(int i = 2; i <= n; i++){
+          int j = 2;
+          int cnt = 0;
+          while(j <= (int)Math.sqrt(i)){
+              if(i % j == 0){
+                cnt += 1;
+                break;
+              } 
+              j += 1;
+          }
+          if(cnt == 0) answer += 1;
+      }
+      return answer;
+  }
 }
