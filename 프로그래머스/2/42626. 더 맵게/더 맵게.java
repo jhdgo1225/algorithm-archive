33939cc0beaeadba625fc355aa03700ab9c17396
JONGHO PARK <94268538+jhdgo1225@users.noreply.github.com>
2026-05-28 17:40:55 +0900

    [level 2] Title: 더 맵게, Time: 964.86 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,6 +1,29 @@
 import java.util.PriorityQueue;
 
 class Solution {
+    /*
+     * PriorityQueue 자료구조 설명
+     *
+     * PriorityQueue는 우선순위가 높은 데이터가 먼저 나오는 자료구조이다.
+     * Java의 PriorityQueue는 기본적으로 "최소 힙" 구조로 동작한다.
+     *
+     * 즉, 숫자를 넣으면 가장 작은 값이 가장 높은 우선순위를 가지며,
+     * pq.peek() 또는 pq.poll()을 했을 때 가장 작은 값이 먼저 나온다.
+     *
+     * 주요 메서드:
+     * - offer(value): 값을 우선순위 큐에 추가한다.
+     * - peek(): 우선순위가 가장 높은 값을 확인한다. 제거하지는 않는다.
+     * - poll(): 우선순위가 가장 높은 값을 꺼내고 제거한다.
+     * - isEmpty(): 큐가 비어 있는지 확인한다.
+     * - size(): 큐에 들어 있는 원소 개수를 확인한다.
+     *
+     * 이 문제에서는 매번 가장 스코빌 지수가 낮은 음식 2개를 꺼내야 한다.
+     * 일반 배열이나 리스트를 사용하면 매번 최솟값을 직접 찾아야 하지만,
+     * PriorityQueue를 사용하면 가장 작은 값을 빠르게 꺼낼 수 있다.
+     *
+     * 따라서 "가장 작은 값 2개를 반복해서 꺼내는 문제"에 PriorityQueue가 적합하다.
+     */
+
     public int solution(int[] scoville, int K) {
         // 스코빌 지수를 오름차순으로 관리하기 위한 최소 힙
         // PriorityQueue는 기본적으로 가장 작은 값이 먼저 나오는 구조이다.
