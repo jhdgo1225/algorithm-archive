710eb46af84ba93a6a553947067b6ac798f84e3e
JONGHO PARK <94268538+jhdgo1225@users.noreply.github.com>
2026-05-28 17:28:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 38.94 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 41.19 ms
+메모리: 102 MB, 시간: 38.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 28일 10:01:34
+2026년 05월 28일 17:28:26
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,13 +3,38 @@ import java.util.HashMap;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+
+        // 참가자 이름과 등장 횟수를 저장할 HashMap
+        // key: 참가자 이름
+        // value: 해당 이름을 가진 참가자의 수
         HashMap<String, Integer> map = new HashMap<>();
+
+        // 참가자 배열을 순회하면서 이름별 참가자 수를 카운트한다.
+        // 동명이인이 있을 수 있으므로 이름이 이미 존재하면 개수를 1 증가시킨다.
         for (String person : participant) {
-            if (map.containsKey(person)) map.put(person, map.get(person) + 1);
-            else map.put(person, 1);
+            if (map.containsKey(person)) {
+                map.put(person, map.get(person) + 1);
+            } else {
+                map.put(person, 1);
+            }
         }
-        for (String person : completion) if (map.containsKey(person)) map.put(person, map.get(person) - 1);
-        for (String person : participant) if (map.get(person) != 0) answer = person;
+
+        // 완주자 배열을 순회하면서 해당 이름의 참가자 수를 1 감소시킨다.
+        // 완주한 사람은 참가자 목록에서 제거된 것과 같은 의미이다.
+        for (String person : completion) {
+            if (map.containsKey(person)) {
+                map.put(person, map.get(person) - 1);
+            }
+        }
+
+        // 참가자 배열을 다시 순회하면서 값이 0이 아닌 사람을 찾는다.
+        // 값이 0이 아니라는 것은 참가자는 있지만 완주하지 못했다는 의미이다.
+        for (String person : participant) {
+            if (map.get(person) != 0) {
+                answer = person;
+            }
+        }
+
         return answer;
     }
 }
